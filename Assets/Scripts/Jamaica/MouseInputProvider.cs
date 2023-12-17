9cabe2cb48721252b21177691e7d00abf8fa358e
takuto0404 <takutomed@gmail.com>
2023-12-17 10:53:15 +0900

    refactor:MouseInputProviderクラス

--- Assets/Scripts/Jamaica/MouseInputProvider.cs
+++ Assets/Scripts/Jamaica/MouseInputProvider.cs
@@ -9,15 +9,16 @@ namespace Jamaica
     {
         [SerializeField] private InputAction pressAction;
         [SerializeField] private InputAction releaseAction;
+        public Vector2 mousePosition;
         private bool _isMouseGettingDown = false;
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         private static void Initialize()
         {
-            Instance.Init();
+            Instance.Initialize_Instance();
         }
-    
-        private void Init()
+
+        private void Initialize_Instance()
         {
             pressAction.Enable();
             releaseAction.Enable();
@@ -25,41 +26,30 @@ namespace Jamaica
             pressAction.performed += OnPress;
             releaseAction.performed += OnRelease;
         }
-    
-
-        public void OnPress(InputAction.CallbackContext context)
-        {
-            if(context.performed)_isMouseGettingDown = true;
-        }
-
-        public void OnRelease(InputAction.CallbackContext context)
-        {
-            if(context.performed)_isMouseGettingDown = false;
-        }
-
-        public Vector2 mousePosition;
 
         public void GetMousePosition(InputAction.CallbackContext context)
         {
             mousePosition = context.ReadValue<Vector2>();
         }
 
-        /// <summary>
-        /// マウスが押される/タップされるまで待つ
-        /// </summary>
-        /// <param name="gameCt"></param>
         public async UniTask OnHoldDownAsync(CancellationToken gameCt)
         {
             await UniTask.WaitUntil(() => _isMouseGettingDown, cancellationToken: gameCt);
         }
 
-        /// <summary>
-        /// マウスが離される/タップが終わるまで待つ
-        /// </summary>
-        /// <param name="gameCt"></param>
         public async UniTask OnHoldUpAsync(CancellationToken gameCt)
         {
             await UniTask.WaitWhile(() => _isMouseGettingDown, cancellationToken: gameCt);
+        }
+
+        public void OnPress(InputAction.CallbackContext context)
+        {
+            if (context.performed) _isMouseGettingDown = true;
+        }
+
+        public void OnRelease(InputAction.CallbackContext context)
+        {
+            if (context.performed) _isMouseGettingDown = false;
         }
     }
 }
